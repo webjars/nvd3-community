2962599d02ca5f5c022c19853982781dec117ed8
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2014-03-27 15:16:55 +0100

    Upgrading to new requireJS version, updated d3 dependency for correct requireJS handling

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -2,7 +2,11 @@
 
 // Ensure any request for this webjar brings in jQuery.
 requirejs.config({
-    shim: {
-        'nv.d3': [ 'webjars!d3.js' ]
+    paths: { "nvd3": webjars.path("nvd3", "nv.d3") },
+    shim: { 
+        'nvd3':  { 
+            'deps' : [ 'd3js'],
+            'exports': 'nv'
+        } 
     }
 });
